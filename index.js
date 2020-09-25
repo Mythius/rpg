4717fe9107b9f93baff5f5b1233cac0213a1fc55
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2020-09-24 21:55:17 -0600

    Create Desktop Application

---
+++ index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    }
+    // icon: __dirname + '/site/favicon.ico'
+  })
+
+  win.loadFile('index.html')
+
+  win.setMenu(null);
+}
+
+app.on('ready', createWindow)
