7aa5a4ab9c1fcb729d5f051cb10f1257f502d0b1
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2020-11-12 20:19:39 -0700

    Add Desktop Shortcut

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const { app, BrowserWindow } = require('electron');
+const createDesktopShortcut = require('create-desktop-shortcuts');
 
-function createWindow () {
-  let win = new BrowserWindow({
-    width: 600,
-    height: 400,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    icon: __dirname + '/favicon.ico'
-  })
+if (process.argv.length > 1 && process.argv[1] != '.') {
+    const shortcutsCreated = createDesktopShortcut({
+        windows: { 
+            filePath: '%appdata%\\..\\local\\chemicc\\chemicc.exe',
+            icon: __dirname + '\\favicon.ico'
+        }
+    });
+}
 
-  win.loadFile('index.html')
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 600,
+        height: 400,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: __dirname + '/favicon.ico'
+    })
 
-  win.setMenu(null);
+    win.loadFile('index.html')
+
+    win.setMenu(null);
 }
 
 app.on('ready', createWindow)
